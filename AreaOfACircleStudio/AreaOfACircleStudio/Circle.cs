1dc55e99f7f6c093a7fd42b79562880eca5d2b99
Sam <samtpomarico@gmail.com>
2020-10-06 20:14:39 -0500

    Studio 1 complete

---
+++ AreaOfACircleStudio/AreaOfACircleStudio/Circle.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AreaOfACircleStudio
+{
+    public class Circle
+    {
+        public static int getRadius(string radius)
+        {
+            int radiusInput = int.Parse(radius);
+            while (radiusInput <= 0)
+            {
+                Console.WriteLine("Invalid input. Please enter positive number:");
+                Console.ReadLine();
+            }
+            return radiusInput;
+        }
+        public static double getCircleArea(double radius)
+        {
+            double radiusInput = radius;
+            return Math.PI * radiusInput * radiusInput;
+        }
+        public static double getCircleCircumference(double radius)
+        {
+            double circumferenceOfCircle = 2 * Math.PI * radius;
+            return circumferenceOfCircle;
+        }
+        public static double getCircleDiameter(double radius)
+        {
+            double diameterOfCircle = 2 * radius;
+            return diameterOfCircle;
+        }
+        
+            
+        
+    }
+}
---
+++ AreaOfACircleStudio/AreaOfACircleStudio/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace AreaOfACircleStudio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please provide radius of your circle:");
+            double radius = Circle.getRadius(Console.ReadLine());
+            Console.WriteLine("The area of a cricle of radius " + radius + " is: " + Circle.getCircleArea(radius) + ". The circumference is: "
+                + Circle.getCircleCircumference(radius) + ", and the diameter is: " + Circle.getCircleDiameter(radius) + ".");
+            Console.WriteLine("What is the MPG of your car?");
+            int milesPerGallon = int.Parse(Console.ReadLine());
+            double gasUsed = Circle.getCircleCircumference(radius) / milesPerGallon;
+            Console.WriteLine("It will take " + gasUsed + "gallons of gas to go around the circle.");
+        }
+    }
+}
